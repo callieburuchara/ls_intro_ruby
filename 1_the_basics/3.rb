95e449ea09dbe3d4a3385207c41098c0aef9c097
Callie Buruchara <callieburuchara@gmail.com>
2020-01-16 08:52:03 -0500

    Completed the basics

---
+++ 1_the_basics/3.rb
@@ -0,0 +1,11 @@
+movies = {
+  :slumdog_millionaire => 2008,
+  :amazing_grace => 2006,
+  :the_little_mermaid => 1989,
+  :the_fellowship_of_the_ring => 2001,
+  :little_women => 1994
+} 
+
+movies.each do |x, y| puts y end
+
+  # I realize I could do this the longer way, by putting each name, but I remembered this from CodeAcademy, so chose to do it the quicker way. 
---
+++ 1_the_basics/4.rb
@@ -0,0 +1,13 @@
+dates = [2008, 2006, 1989, 2001, 1994]
+
+puts dates
+
+# or using an index...
+
+dates = [2008, 2006, 1989, 2001, 1994]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
